effa3f3d542780922c6a069c5239f92b1fa26689
Jesse Estes <jestes5111@gmail.com>
2019-02-16 13:37:39 -0500

    Fixed merge issues

--- src/main/java/frc/robot/commands/drivetrain/CurvatureDrive.java
+++ src/main/java/frc/robot/commands/drivetrain/CurvatureDrive.java
@@ -11,6 +11,9 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
 public class CurvatureDrive extends Command {
+
+  private final double SPEED_MULTIPLIER = 0.75;
+  private final double ROTATION_MULTIPLIER = 0.5;
   public CurvatureDrive() {
     requires(Robot.drivetrain);
   }
@@ -23,8 +26,8 @@ public class CurvatureDrive extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.drivetrain.curvatureDrive(Robot.oi.getRightJoystickY(), 
-                                    Robot.oi.getLeftJoystickX());
+    Robot.drivetrain.curvatureDrive(Robot.oi.getRightJoystickY() * SPEED_MULTIPLIER, 
+                                    Robot.oi.getLeftJoystickX() * ROTATION_MULTIPLIER);
   }
 
   // Make this return true when this Command no longer needs to run execute()
--- src/main/java/frc/robot/commands/drivetrain/CurvatureDriveFullSpeed.java
+++ src/main/java/frc/robot/commands/drivetrain/CurvatureDriveFullSpeed.java
@@ -23,8 +23,8 @@ public class CurvatureDriveFullSpeed extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.drivetrain.curvatureDrive(Robot.oi.getLeftX(), 
-                                    Robot.oi.getRightY());
+    Robot.drivetrain.curvatureDrive(Robot.oi.getRightJoystickY(),
+                                    Robot.oi.getLeftJoystickX());
   }
 
   // Make this return true when this Command no longer needs to run execute()
