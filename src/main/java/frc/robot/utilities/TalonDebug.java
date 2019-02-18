9fd41442f39ef2ba4eb7b328584f8a47ba804ee6
Nathaniel Johnson <nathancj224@gmail.com>
2019-02-18 13:16:29 -0500

    improve TalonDebug print

--- src/main/java/frc/robot/utilities/TalonDebug.java
+++ src/main/java/frc/robot/utilities/TalonDebug.java
@@ -11,13 +11,13 @@ public class TalonDebug {
     public static void printSrxClosedLoopValues(TalonSRX talonSrx, String name) {
         System.out.println(
             (name == null ? "" : "[" + name + "] ") +
-            "[" + talonSrx.getDeviceID() + "]: " + 
-            "Voltage = " + talonSrx.getMotorOutputVoltage() + ", " +
-            "Sensor Pos = " + talonSrx.getSelectedSensorPosition() + ", " +
-            "Target = " + talonSrx.getClosedLoopTarget() + ", " +
-            "Error = " + talonSrx.getClosedLoopError() + ", " +
-            "Error Derivative = " + talonSrx.getErrorDerivative() + ", " +
-            "Integral Accumulator = " + talonSrx.getIntegralAccumulator()
+            "[ID" + talonSrx.getDeviceID() + "]: " + 
+            " Voltage = " + talonSrx.getMotorOutputVoltage() +
+            " Sensor Pos = " + talonSrx.getSelectedSensorPosition() +
+            " Target = " + talonSrx.getClosedLoopTarget() +
+            " Error = " + talonSrx.getClosedLoopError() +
+            " Error Derivative = " + talonSrx.getErrorDerivative() +
+            " Integral Accumulator = " + talonSrx.getIntegralAccumulator()
         );
     }
 
